4533505fcfda006473ffa24a7f697f26b526ac5e
Ben Boeckel <mathstuf@gmail.com>
2014-11-01 19:32:19 -0400

    boot: remove excess juggling on Windows

--- src/abagames/tt/boot.d
+++ src/abagames/tt/boot.d
@@ -28,43 +28,9 @@ GameManager gameManager;
 PrefManager prefManager;
 MainLoop mainLoop;
 
-version (Win32_release) {
-  // Boot as the Windows executable.
-  private import std.c.windows.windows;
-  private import std.string;
-
-  extern (C) void gc_init();
-  extern (C) void gc_term();
-  extern (C) void _minit();
-  extern (C) void _moduleCtor();
-
-  extern (Windows)
-  public int WinMain(HINSTANCE hInstance,
-                     HINSTANCE hPrevInstance,
-                     LPSTR lpCmdLine,
-                     int nCmdShow) {
-    int result;
-    gc_init();
-    _minit();
-    try {
-      _moduleCtor();
-      char exe[4096];
-      GetModuleFileNameA(null, exe, 4096);
-      string[1] prog;
-      prog[0] = std.string.toString(exe);
-      result = boot(prog ~ std.string.split(std.string.toString(lpCmdLine)));
-    } catch (Throwable o) {
-      Logger.error("Exception: " ~ o.toString());
-      result = EXIT_FAILURE;
-    }
-    gc_term();
-    return result;
-  }
-} else {
-  // Boot as the general executable.
-  public int main(string[] args) {
-    return boot(args);
-  }
+// Boot as the general executable.
+public int main(string[] args) {
+  return boot(args);
 }
 
 public int boot(string[] args) {
